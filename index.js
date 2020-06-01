193a43f5f389dba1cad91f1b061616beed417ea1
Frederic Brodbeck <der.froedi@gmail.com>
2020-06-01 20:56:24 +0200

    refactor

--- index.js
+++ index.js
@@ -2,43 +2,61 @@ const { exec } = require('child_process');
 const R = require('ramda');
 
 
-function getTags(rootDir) {
+function asyncExec(command) {
 	return new Promise((resolve, reject) => {
 		exec(
-			`ag \
-				--only-matching \
-				--no-filename \
-				--nonumbers \
-				-i '#[a-z]+' \
-				"${rootDir}" \
-					| awk NF \
-					| sort \
-					| uniq -c \
-					| sort --reverse`,
+			command,
 			{},
 			(err, stdout, stderr) => {
 				if (err) {
 					console.error(err);
 					return reject(err);
 				}
-				const lines = stdout.split(/[\r\n]/g)
-					.map((line) => {
-						const l = `${line}`.trim();
-						return (!l.length) ? null : l;
-					})
-					.filter(R.identity);
-				const result = lines
-					.map((line) => {
-						const [count, label] = line.trim().split(/[ \t]+/g);
-						return {
-							count: parseInt(count, 10),
-							label,
-						};
-					});
-				resolve(result);
+				resolve({ stdout, stderr });
 			}
 		);
-	})
+	});
+}
+
+
+function getExecStdout(command) {
+	return asyncExec(command)
+		.then(({ stdout, stderr }) => {
+			const lines = stdout.split(/[\r\n]/g)
+				.map((line) => {
+					const l = `${line}`.trim();
+					return (!l.length) ? null : l;
+				})
+				.filter(R.identity);
+			return lines;
+		});
+}
+
+
+async function getTags(rootDir) {
+	const command = [
+		'ag',
+			'--only-matching',
+			'--no-filename',
+			'--nonumbers',
+			'-i \'#[a-z]+\'',
+			`"${rootDir}"`,
+				'| awk NF', // remove empty lines
+				'| sort',
+				'| uniq -c',
+				'| sort --reverse', // most frequent first
+	].join(' ');
+
+	const parseLine = (line) => {
+		const [count, label] = line.trim().split(/[ \t]+/g);
+		return {
+			count: parseInt(count, 10),
+			label,
+		};
+	};
+
+	return getExecStdout(command)
+		.then((lines) => lines.map(parseLine));
 }
 
 
