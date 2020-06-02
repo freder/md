c1984d75acc78546ac196d93483d077bca34f359
Frederic Brodbeck <der.froedi@gmail.com>
2020-06-02 10:16:01 +0200

    draggable ndoes

--- web/index.js
+++ web/index.js
@@ -36,14 +36,44 @@ fetch('data.json')
 			.data(links)
 			.enter()
 				.append('line')
-				.attr('stroke', '#999')
+				.attr('stroke', '#999');
 
+		const drag = (simulation) => {
+			function dragstarted(d) {
+				if (!d3.event.active) {
+					simulation
+						.alphaTarget(0.3)
+						.restart();
+				}
+				d.fx = d.x;
+				d.fy = d.y;
+			}
+			
+			function dragged(d) {
+				d.fx = d3.event.x;
+				d.fy = d3.event.y;
+			}
+			
+			function dragended(d) {
+				if (!d3.event.active) {
+					simulation.alphaTarget(0);
+				}
+				d.fx = null;
+				d.fy = null;
+			}
+			
+			return d3.drag()
+				.on('start', dragstarted)
+				.on('drag', dragged)
+				.on('end', dragended);
+		};
 
 		const node = svg.append('g')
 			.selectAll('g')
 			.data(nodes)
 			.enter()
-				.append('g');
+				.append('g')
+				.call(drag(simulation));
 
 		const circles = node.append('circle')
 			.attr('r', 5)
